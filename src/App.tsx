b48772f3c5209edb30e6263f4e821eb5264d08f0
vlad1chek <vladbarcelonafc@gmail.com>
2021-03-05 14:07:40 +0200

    chore: prepare project to the development process

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { StrictMode } from "react";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StrictMode>
+      <div className="App">
+        <form className="App-header">
+          <TextField id="standard-basic" label="Standard" />
+        </form>
+      </div>
+    </StrictMode>
   );
 }
 
