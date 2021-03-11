0e7861c827fa7fcaec9dc6a9dde90f8f879e4298
vlad1chek <vladbarcelonafc@gmail.com>
2021-03-11 09:49:15 +0200

    feat: add web3-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Web3ReactProvider } from "@web3-react/core";
 import "./index.css";
 import App from "./App";
+import { getLibrary } from "./utils/helpers";
 
 function SatoshiArt() {
-  return <App />;
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  );
 }
 
 ReactDOM.render(<SatoshiArt />, document.getElementById("root"));
