6716399d93ab3d31a961bb98316174c6ab590985
Taras Paliy <taras505paliy@gmail.com>
2021-03-12 18:47:26 +0200

    lint fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Web3ReactProvider } from "@web3-react/core";
-import "./index.css";
-import App from "./App";
-import { getLibrary } from "./utils/helpers";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Web3ReactProvider } from '@web3-react/core'
+import './index.css'
+import App from './App'
+import { getLibrary } from './utils/helpers'
 
 function SatoshiArt() {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
-    </Web3ReactProvider>
-  );
+    return (
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <App />
+        </Web3ReactProvider>
+    )
 }
 
-ReactDOM.render(<SatoshiArt />, document.getElementById("root"));
+ReactDOM.render(<SatoshiArt />, document.getElementById('root'))
