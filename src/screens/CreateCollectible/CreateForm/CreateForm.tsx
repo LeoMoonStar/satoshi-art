e3c589aea46cb577369f4de2133c5ed9537323e1
Vitaliy Mischenko <vitaliy.m@team.anyforsoft.com>
2021-04-07 21:32:58 +0300

    add toke type

--- src/screens/CreateCollectible/CreateForm/CreateForm.tsx
+++ src/screens/CreateCollectible/CreateForm/CreateForm.tsx
@@ -295,6 +295,9 @@ const CreateForm = ({ isSingle }: { isSingle: boolean }): JSX.Element => {
         if (!chainId) {
             return
         }
+        if (!account) {
+            return
+        }
         setIsSubmitModal(true)
         const formData = new FormData()
         formData.append('files', data.file[0])
@@ -302,7 +305,7 @@ const CreateForm = ({ isSingle }: { isSingle: boolean }): JSX.Element => {
             formData.append('files', data.cover[0])
         }
         const [fileResponse, coverResponse] = await uploadFile(formData)
-
+        const type = isSingle ? TokenType.SINGLE : TokenType.MULTIPLE
         const metadata = {
             name: data.name,
             description: data.description,
@@ -314,7 +317,7 @@ const CreateForm = ({ isSingle }: { isSingle: boolean }): JSX.Element => {
             type: isSingle ? TokenType.SINGLE : TokenType.MULTIPLE,
         }
 
-        const metaResponse = await uploadMetaData(metadata)
+        const metaResponse = await uploadMetaData(metadata, account, type)
         const { response, tokenType } = await createItem(metaResponse.payload)
         await updateMetaData(metaResponse.id, response.hash)
         dispatch(
