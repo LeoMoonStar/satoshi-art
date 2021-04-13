51ae45ccb9237227da7710e181a9a6544f7889ce
Viktor Panasiuk <viktor.p@team.anyforsoft.com>
2021-04-13 09:31:04 +0300

    Static view following and followers

--- src/screens/UserDashboard/Tokens/FollowingAndFollowers/FollowingAndFollowers.tsx
+++ src/screens/UserDashboard/Tokens/FollowingAndFollowers/FollowingAndFollowers.tsx
@@ -2,20 +2,15 @@ import React from 'react'
 
 import Avatar from 'shared/Avatar'
 import Button from 'shared/Button'
+import { LikeIcon, ViewsIcon } from 'shared/icons'
 
 import useStyles from './FollowingAndFollowers.style'
 
 import preview from 'shared/images/artist/work.jpg'
-const mockTokens = Array.from({ length: 24 }, (index) => ({
-    id: index,
-    preview,
-    name: 'Fresh Meat #F',
-    author: {
-        image: '',
-        name: 'Fimbim',
-        price: '124.56x3 ETH',
-    },
-}))
+const items: number[] = Array.from(
+    { length: 5 },
+    (index: number): number => index
+)
 
 const Item = () => {
     const classes = useStyles()
@@ -25,8 +20,31 @@ const Item = () => {
             <Avatar image={preview} size={84} />
             <div className={classes.itemInfo}>
                 <div className={classes.topRow}>
-                    <b>Flimbim</b>
-                    <Button variantCustom="outlined">Following</Button>
+                    <b className={classes.userName}>Flimbim</b>
+                    <Button
+                        variantCustom="outlinedLink"
+                        className={classes.followingButton}
+                    >
+                        Following
+                    </Button>
+                </div>
+                <div className={classes.location}>Berlin, Germany</div>
+                <div className={classes.balance}>124.563 ETH</div>
+                <div className={classes.bottomRow}>
+                    <div className={classes.bottomCol}>
+                        <LikeIcon /> 21,0k
+                    </div>
+                    <div className={classes.bottomCol}>
+                        <ViewsIcon /> 216,8k
+                    </div>
+                </div>
+            </div>
+            <div className={classes.previewWorks}>
+                <div className={classes.previewWork}>
+                    <img src={preview} alt="" />
+                </div>
+                <div className={classes.previewWork}>
+                    <img src={preview} alt="" />
                 </div>
             </div>
         </div>
@@ -39,10 +57,25 @@ export default function Collections(): JSX.Element {
         <div className={classes.container}>
             <div className={classes.col}>
                 <h2 className={classes.title}>Following</h2>
-                <Item />
+                <div className={classes.items}>
+                    {items.map((item) => (
+                        <Item key={item} />
+                    ))}
+                    <Button variantCustom="action" className={classes.viewAll}>
+                        View All
+                    </Button>
+                </div>
             </div>
             <div className={classes.col}>
                 <h2 className={classes.title}>Followers</h2>
+                <div className={classes.items}>
+                    {items.map((item) => (
+                        <Item key={item} />
+                    ))}
+                    <Button variantCustom="action" className={classes.viewAll}>
+                        View All
+                    </Button>
+                </div>
             </div>
         </div>
     )
