392a8816f7becdd88ac442068cceb8912f52bfba
Vitaliy Mischenko <vitaliy.m@team.anyforsoft.com>
2021-04-22 13:58:47 +0300

    pr fix

--- src/shared/Works/Works.tsx
+++ src/shared/Works/Works.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import { IconButton } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 
+import { useCurrentNetwork } from 'hooks/useCurrentNetwork'
 import Button from 'shared/Button'
 // import { SaveIcon, ViewsIcon } from 'shared/icons'
 // import Avatar from 'shared/Avatar'
@@ -29,6 +30,8 @@ export default function WorksList({
     isLoading = true,
     tokens = [],
 }: WorksListProps): JSX.Element {
+    const { explorer } = useCurrentNetwork()
+
     const classes = useStyles()
     const { t } = useTranslation()
 
@@ -100,7 +103,7 @@ export default function WorksList({
                                         <a
                                             target="_blank"
                                             rel="noreferrer"
-                                            href={`https://ropsten.etherscan.io/address/${walletHash}`}
+                                            href={`${explorer}/address/${walletHash}`}
                                         >
                                             {shortAddress(walletHash)}
                                         </a>
