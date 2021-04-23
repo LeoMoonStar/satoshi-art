eae097017bdf1c038c8e0132149d0c5c802e4f64
Igor <probleminu@gmail.com>
2021-04-23 10:48:53 +0300

    added DropOfTheDaySliderItem

--- src/shared/DropOfTheDaySlider/DropOfTheDaySlider.tsx
+++ src/shared/DropOfTheDaySlider/DropOfTheDaySlider.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import Slider from 'react-slick'
-import { IconButton } from '@material-ui/core'
-import { useTranslation } from 'react-i18next'
 import cx from 'clsx'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-import bradPitPaintImage from 'shared/images/home/bradPitPaint.png'
-import Avatar from 'shared/Avatar'
-import Button from 'shared/Button'
-import { SliderArrow, SaveIcon, ViewsIcon } from 'shared/icons'
+import { SliderArrow } from 'shared/icons'
 import useStyles from './DropOfTheDaySlider.style'
-import useWalletTokens from './../../hooks/useWalletTokens'
-import { isTokenOwned } from 'utils/common'
+import DropOfTheDaySliderItem from './DropOfTheDaySliderItem/DropOfTheDaySliderItem'
 
 const dropsOfTheDay = [
     { id: '6080107c6aeffc0014c8df3d' }, //hardcoded one of my Token.id
@@ -71,68 +65,12 @@ export default function DropOfTheDaySlider({
     onSeeAll,
 }: DropOfTheDaySliderProps): JSX.Element {
     const classes = useStyles()
-    const { t } = useTranslation()
-    const userTokens = useWalletTokens()
 
     return (
         <Slider className={cx(classes.slider, className)} {...sliderConfig}>
             {dropsOfTheDay.map((el, index) => (
                 <div key={index}>
-                    <div className={classes.slide}>
-                        <img
-                            src={bradPitPaintImage}
-                            alt=""
-                            className={classes.slideImage}
-                        />
-                        <div className={classes.info}>
-                            <Avatar
-                                alt="Fresh meat"
-                                status="premium"
-                                size={60}
-                                image={bradPitPaintImage}
-                            />
-                            <div className={classes.header}>
-                                <h3>Fresh Meat #F</h3>
-                                <div className={classes.actionButtons}>
-                                    <IconButton
-                                        className={classes.actionButton}
-                                    >
-                                        <SaveIcon />
-                                    </IconButton>
-                                    <IconButton
-                                        className={classes.actionButton}
-                                    >
-                                        <ViewsIcon />
-                                    </IconButton>
-                                </div>
-                            </div>
-                            <div className={classes.cardContent}>
-                                <div className={classes.authorInfo}>
-                                    <a href="">@Fimbim</a> 124.56x3 ETH
-                                </div>
-                                <div className={classes.workInfo}>
-                                    0.25 ETH
-                                    <span className={classes.count}>
-                                        1 of 1
-                                    </span>
-                                    {isTokenOwned(el.id, userTokens) ? null : (
-                                        <Button className={classes.bidButton}>
-                                            {t('placeABid')}
-                                        </Button>
-                                    )}
-                                </div>
-                                {onSeeAll && (
-                                    <Button
-                                        onClick={onSeeAll}
-                                        className={classes.seeAll}
-                                        variantCustom="action"
-                                    >
-                                        {t('seeAll')}
-                                    </Button>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                    <DropOfTheDaySliderItem id={el.id} />
                 </div>
             ))}
         </Slider>
